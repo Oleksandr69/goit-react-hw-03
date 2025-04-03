73b6fbe037c576109b5254453e0aaea68d2b9587
Oleksandr Shevchenko <shevchol69@gmail.com>
2025-04-03 12:30:09 +0300

    nanoid and yup added

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,22 +1,32 @@
 import { useId } from "react";
 import React from "react";
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { nanoid } from "nanoid";
+import * as Yup from "yup";
 
 
-const initialValues = {
-  name: "",
-  number: "",
-};
 
 const ContactForm = ({onAdd}) => {
   const nameFieldId = useId();
   const telFieldId = useId();
 
+  const initialValues = {
+    name: "",
+    number: "",
+  };
+
+  const FeedbackSchema = Yup.object().shape({
+  name: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required"),
+  number: Yup.number().required("Required"),
+  // message: Yup.string().min(3, "Too short").max(256, "Too long").required("Required"),
+  // level: Yup.string().oneOf(["good", "neutral", "bad"]).required("Required")
+});
+
   const handleSubmit = (values, actions) => {
     console.log(values);
 
     onAdd({
-      id: Date.now(),
+      id: nanoid(),
       name: values.name,
       number: values.number,
     });
@@ -25,13 +35,20 @@ const ContactForm = ({onAdd}) => {
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+    <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={FeedbackSchema}>
       <Form>
-        <label htmlFor={nameFieldId}>Name</label>
-        <Field type="text" name="name" id={nameFieldId} />
+        
+        <div>
+          <label htmlFor={nameFieldId}>Name</label>
+          <Field type="text" name="name" id={nameFieldId} />
+          <ErrorMessage name="name" component="span" />
+        </div>
 
-        <label htmlFor={telFieldId}>Number</label>
-        <Field type='text' name="number" id={telFieldId} />
+        <div>
+          <label htmlFor={telFieldId}>Number</label>
+          <Field type='text' name="number" id={telFieldId} />
+          <ErrorMessage name="number" component="span" />
+        </div>
 
         <button type="submit">Add contact</button>
       </Form>
