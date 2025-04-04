64aefef4ba20b94dee531fcf57bb715a14649217
Oleksandr Shevchenko <shevchol69@gmail.com>
2025-04-04 15:32:21 +0300

    FaPhoneAlt

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,12 @@
 import css from './Contact.module.css';
-import { FaUser, FaPhone } from "react-icons/fa";
+import { FaUser, FaPhoneAlt } from "react-icons/fa";
 
 export default function Contact({ contact, onDelete}) {
   return (
     <div className={css.item}>
       <div>
         <p className={css.text}><FaUser/>     {contact.name} </p>
-        <p className={css.text}><FaPhone/>     {contact.number} </p>
+        <p className={css.text}><FaPhoneAlt/>     {contact.number} </p>
       </div>
       <button className={css.btn} onClick={() => onDelete(contact.id)}>
         Delete
